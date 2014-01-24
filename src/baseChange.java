8e002398c662fd5e607053aed174923835265581
Amalan Iyengar <amalan.iyengar@gmail.com>
2014-01-24 12:45:24 -0500

    Added encoding and decoding of ASCII strings

--- src/baseChange.java
+++ src/baseChange.java
@@ -1,6 +1,8 @@
 import java.lang.Math;
 public class baseChange {
-	private static String reverse(String string){
+	private final int PAD_LEFT=0;
+	private final int PAD_RIGHT=1;
+	private String reverse(String string){
 		char[] digits=string.toCharArray();
 		for(int i=0;i<digits.length/2;i++){
 			char temp=digits[i];
@@ -10,7 +12,7 @@ public class baseChange {
 		return new String(digits);
 	}
 	//Yes, I know this can be accomplished by a simple 2-line function, but the point is to learn, no?
-	public static String convert(String num, int from, int to){
+	public String convert(String num, int from, int to){
 		if(from>36 || from<2){
 			throw new IllegalArgumentException("Starting base must be between 2 and 36");
 		}
@@ -45,5 +47,56 @@ public class baseChange {
 		
 		
 	}
-	
+	public int toBase10(String num, int from){
+		if(from>36 || from<2){
+			throw new IllegalArgumentException("Starting base must be between 2 and 36");
+		}
+		char[] digits=num.toCharArray();
+		int length=digits.length-1;
+		int sum=0;
+		
+		for(char c:digits){
+			int i=Character.getNumericValue(c);
+			if(i>=from){
+				throw new IllegalArgumentException("That number is invalid for the specified starting base");
+			}
+			sum+=(i*Math.pow(from, length));
+			length--;
+		}
+		return sum;
+	}
+	private String pad(String str,int length,char paddingChar, int type){
+		if(type==PAD_RIGHT){
+			while(str.length()<length){
+				str+=paddingChar;
+			}
+
+		}else if(type==PAD_LEFT){
+			String newStr="";
+			for(int i=0;i<length-str.length();i++){
+				newStr+=paddingChar;
+			}
+			str=newStr+str;
+		}
+		return str;
+	}
+	public String encodeASCII(String ASCII,int to){
+		char[] characters=ASCII.toCharArray();
+		String str="";
+		for(char c:characters){
+			str+=(pad(convert(Integer.toString((int)c),10,to),(int)Math.floor(Math.log(128)/Math.log(to)+1),'0',PAD_LEFT));
+		}
+		return str;
+
+		
+	}
+	public String decodeASCII(String numbers, int from){
+		int length=(int)Math.floor(Math.log(128)/Math.log(from)+1);
+		String result="";
+		for(int i=0;i<numbers.length();i+=length){
+			String character=numbers.substring(i,i+length);
+			result+=(char)toBase10(convert(character,from,10),10);
+		}
+		return result;
+	}
 }
